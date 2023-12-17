21aae404ed8d0f9c38061f22a7bf8b8ba7190445
XiNiHa <me@xiniha.dev>
2023-12-17 19:41:39 +0900

    chore: more eslint configurations

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,17 +5,59 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "@unocss",
     "plugin:astro/recommended",
+    "plugin:prettier/recommended",
   ],
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "import", "prettier"],
+  settings: {
+    "import/resolver": {
+      typescript: true,
+      node: true,
+    },
+  },
   overrides: [
     {
       files: ["*.astro"],
+      processor: "astro/client-side-ts",
       parser: "astro-eslint-parser",
       parserOptions: {
         parser: "@typescript-eslint/parser",
         extraFileExtensions: [".astro"],
       },
     },
+    {
+      files: ["**/*.astro/*.ts", "*.astro/*.ts"],
+      env: {
+        browser: true,
+        es2020: true,
+      },
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        sourceType: "module",
+        project: null,
+      },
+      rules: {
+        "prettier/prettier": "off",
+      },
+    },
   ],
+  rules: {
+    "prettier/prettier": "error",
+    "import/order": [
+      "error",
+      {
+        "newlines-between": "always",
+        pathGroups: [
+          {
+            pattern: "@/**",
+            group: "parent",
+          },
+        ],
+        alphabetize: {
+          order: "asc",
+          orderImportKind: "desc",
+        },
+      },
+    ],
+  },
 };
