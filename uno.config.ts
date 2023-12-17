07ac8f15011bd50bd3d1947c499e2c5c2edc92e6
XiNiHa <me@xiniha.dev>
2023-12-17 20:54:09 +0900

    chore: add unocss config

---
+++ uno.config.ts
@@ -0,0 +1,11 @@
+import {
+  defineConfig,
+  presetIcons,
+  presetUno,
+  transformerVariantGroup,
+} from "unocss";
+
+export default defineConfig({
+  presets: [presetUno(), presetIcons()],
+  transformers: [transformerVariantGroup()],
+});
