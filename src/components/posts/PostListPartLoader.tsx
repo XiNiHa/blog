1b0bfc6359afaa443672a466e647d182142b44e7
XiNiHa <me@xiniha.dev>
2023-12-18 02:36:14 +0900

    feat: implement posts page

---
+++ src/components/posts/PostListPartLoader.tsx
@@ -0,0 +1,34 @@
+import { onMount } from "solid-js";
+
+interface Props {
+  after: string;
+}
+
+export default function PostListPartLoader(props: Props) {
+  const el = (<div />) as HTMLDivElement;
+
+  let lastFetch = 0;
+
+  onMount(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry?.isIntersecting && lastFetch < Date.now() - 1000) {
+          lastFetch = Date.now();
+          window.htmx
+            .ajax("GET", `/partials/posts?after=${props.after}`, {
+              source: el,
+              swap: "outerHTML",
+            })
+            .finally(() => observer.disconnect());
+        }
+      },
+      {
+        rootMargin: "80%",
+        threshold: 0.01,
+      },
+    );
+    observer.observe(el);
+  });
+
+  return el;
+}
