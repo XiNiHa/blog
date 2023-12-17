e429534b43d8f17f3b981059f86f776f7e18d023
XiNiHa <me@xiniha.dev>
2023-12-17 21:47:54 +0900

    chore: setup misc utils and icons

--- uno.config.ts
+++ uno.config.ts
@@ -1,3 +1,6 @@
+import { readFileSync } from "node:fs";
+
+import fastGlob from "fast-glob";
 import {
   defineConfig,
   presetIcons,
@@ -6,6 +9,18 @@ import {
 } from "unocss";
 
 export default defineConfig({
-  presets: [presetUno(), presetIcons()],
+  presets: [
+    presetUno(),
+    presetIcons({
+      collections: {
+        my: Object.fromEntries(
+          fastGlob.sync("./src/assets/icons/*.svg").map((path) => {
+            const file = readFileSync(path, "utf-8");
+            return [path.replace(/^.*\/([^/]+)\.svg$/, "$1"), file] as const;
+          }),
+        ),
+      },
+    }),
+  ],
   transformers: [transformerVariantGroup()],
 });
