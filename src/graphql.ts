ead26950449b3bdc634fe2a3d5d14ec5500b4084
XiNiHa <me@xiniha.dev>
2023-12-17 22:11:48 +0900

    fix: use fetch for graphql-request

--- src/graphql.ts
+++ src/graphql.ts
@@ -2,5 +2,6 @@ import { GraphQLClient } from "graphql-request";
 
 export const client = new GraphQLClient("https://gql.hashnode.com", {
   headers: { Authorization: import.meta.env.HASHNODE_TOKEN },
+  fetch,
 });
 export const request = client.request.bind(client);
